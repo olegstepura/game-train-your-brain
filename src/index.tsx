22405ccc30d40e2caca97697d440392fb836eb77
Oleg Stepura <oleg@nezasa.com>
2020-12-07 14:52:28 +0200

    add mobile 100vh fix

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './utils/reportWebVitals';
+import './utils/mobile100vhFix';
 
 ReactDOM.render(
   <React.StrictMode>
